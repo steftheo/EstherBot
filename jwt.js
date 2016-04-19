754b8a10aec6b8c6cefbef83dc8a0e2154971a2d
Stefan Theofilos <StefanTheofilos@Stefans-MacBook-Pro.local>
2016-04-19 15:03:37 -0500

    jwt.js

--- jwt.js
+++ jwt.js
@@ -12,7 +12,31 @@ const jwt = jsonwebtoken.sign({
 
 module.exports = jwt;
 
-// If run directly, print JWT to cmd line 
+// If run directly, print JWT to cmd line
 if (process.argv[1] === __filename) {
     console.log(jwt);
 }
+
+var Botkit = require('botkit');
+
+var accessToken = process.env.FACEBOOK_PAGE_ACCESS_TOKEN;
+var verifyToken = process.env.FACEBOOK_VERIFY_TOKEN;
+var port = process.env.PORT;
+
+if (!accessToken) throw new Error('FACEBOOK_PAGE_ACCESS_TOKEN is required but missing');
+if (!verifyToken) throw new Error('FACEBOOK_VERIFY_TOKEN is required but missing');
+if (!port) throw new Error ('PORT is required but missing');
+
+var controller = Botkit.facebookbot({
+  access_token: accessToken,
+  verify_token: verifyToken
+});
+
+var bot = controller.spawn()
+
+controller.setupWebserver(port, function (err, webserver) {
+  if (err) return console.log(err);
+  controller.createWebhookEndpoints(webserver, bot, function () {
+    console.log('Ready Player 1');
+  });
+});
